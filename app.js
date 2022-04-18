b54d2ac49228b46ccbd39bcd30148c1063a79f22
Emaneliforp <kietchouam@gmail.com>
2022-04-17 20:28:27 -0700

    cache

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const client = new Client({
 		GuildBanManager: 0, // guild.bans
 		GuildInviteManager: 0, // guild.invites
 		GuildManager: Infinity, // client.guilds
-		GuildMemberManager: 0, // guild.members
+		GuildMemberManager: Infinity, // guild.members
 		GuildStickerManager: 0, // guild.stickers
 		MessageManager: 0, // channel.messages
 		PresenceManager: 0, // guild.presences
